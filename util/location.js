8f0a8280a8802ab1f76e17962d4a1d88a8e1a8d6
khalid alasmary <khalid@alasmary.com>
2020-01-05 06:46:00 +0300

    .

--- util/location.js
+++ util/location.js
@@ -2,26 +2,26 @@ const axios = require('axios');
 
 const HttpError = require('../models/error-handler');
 
-const API_KEY = 'AIzaSyCC68KiU89t67N9o553vfaYMGB25Palies';
-
 async function getCoordinatesByAddress(address) {
-  // const response = await axios.get(
-  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-  //     address
-  //   )}&key=${API_KEY}`
-  // );
-  // const data = response.data;
-  // if (!data || data.status === 'ZERO_RESULTS' || data.results.length === 0) {
-  //   throw new HttpError('No Place found with this Address', 422);
-  // }
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${process.env.GOOGLE_API_KEY}`
+  );
+  const data = response.data;
+  if (!data || data.status === 'ZERO_RESULTS' || data.results.length === 0) {
+    throw new HttpError('No Place found with this Address', 422);
+  }
 
   // console.log(data);
-  // const coordinates = data.results[0].geometry.location;
+  const coordinates = data.results[0].geometry.location;
 
-  return {
-    lat: 24.9417598,
-    lng: 46.712393
-  };
+  return coordinates;
+  // IF YOU DONT HAVE GOOGLE API KEY YOU CAN RETURN DUMMY LOCATION
+  // return {
+  //   lat: 24.9417598,
+  //   lng: 46.712393
+  // };
 }
 
 module.exports = getCoordinatesByAddress;
